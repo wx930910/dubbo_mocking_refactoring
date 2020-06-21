e1324f663efedf57700a89c5c43115d3d939eb5c
TwilightSparks <wx19930910@gmail.com>
2020-06-21 17:07:03 -0400

    Mocking NullInvocation.class in CacheTest.java

---
+++ dubbo-compatible/src/test/java/org/apache/dubbo/cache/CacheTestWithMock.java
@@ -0,0 +1,46 @@
+package org.apache.dubbo.cache;
+
+import org.apache.dubbo.rpc.RpcInvocation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.alibaba.dubbo.cache.Cache;
+import com.alibaba.dubbo.cache.CacheFactory;
+import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.rpc.Invocation;
+
+public class CacheTestWithMock {
+	@Test
+	public void testCacheFactory() {
+		URL url = URL.valueOf(
+				"test://test:11/test?cache=jacache&.cache.write.expire=1");
+		CacheFactory cacheFactory = new MyCacheFactory();
+		Invocation invocation = Mockito.mock(Invocation.class);
+		Mockito.doReturn(null).when(invocation).getTargetServiceUniqueName();
+		Mockito.doReturn(null).when(invocation).getMethodName();
+		Mockito.doReturn(new Class[0]).when(invocation).getParameterTypes();
+		Mockito.doReturn(new Object[0]).when(invocation).getArguments();
+		Mockito.doReturn(null).when(invocation).getAttachments();
+		Mockito.doReturn(null).when(invocation)
+				.getAttachment(Mockito.anyString());
+		Mockito.doReturn(null).when(invocation)
+				.getAttachment(Mockito.anyString(), Mockito.anyString());
+		Mockito.doReturn(null).when(invocation).getInvoker();
+		Mockito.doReturn(null).when(invocation).put(Mockito.any(Object.class),
+				Mockito.any(Object.class));
+		Mockito.doReturn(null).when(invocation).get(Mockito.any(Object.class));
+		Mockito.doReturn(null).when(invocation).getAttributes();
+
+		Cache cache = cacheFactory.getCache(url, invocation);
+		cache.put("testKey", "testValue");
+
+		org.apache.dubbo.cache.CacheFactory factory = cacheFactory;
+		org.apache.dubbo.common.URL u = org.apache.dubbo.common.URL.valueOf(
+				"test://test:11/test?cache=jacache&.cache.write.expire=1");
+		org.apache.dubbo.rpc.Invocation inv = new RpcInvocation();
+		org.apache.dubbo.cache.Cache c = factory.getCache(u, inv);
+		String v = (String) c.get("testKey");
+		Assertions.assertEquals("testValue", v);
+	}
+}
