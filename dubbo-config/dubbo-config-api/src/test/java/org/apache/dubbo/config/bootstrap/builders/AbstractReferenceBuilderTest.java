d484acae38b2a566a59f5be3834a222cb76974dd
TwilightSparks <wx19930910@gmail.com>
2020-06-21 16:50:54 -0400

    Mocking ReferenceConfig.class in AbstractReferenceBuilderTest.java

--- dubbo-config/dubbo-config-api/src/test/java/org/apache/dubbo/config/bootstrap/builders/AbstractReferenceBuilderTest.java
+++ dubbo-config/dubbo-config-api/src/test/java/org/apache/dubbo/config/bootstrap/builders/AbstractReferenceBuilderTest.java
@@ -16,136 +16,145 @@
  */
 package org.apache.dubbo.config.bootstrap.builders;
 
-import org.apache.dubbo.config.AbstractReferenceConfig;
+import static org.apache.dubbo.common.constants.CommonConstants.GENERIC_SERIALIZATION_BEAN;
 
+import org.apache.dubbo.config.AbstractReferenceConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.apache.dubbo.common.constants.CommonConstants.GENERIC_SERIALIZATION_BEAN;
+import org.mockito.Mockito;
 
 class AbstractReferenceBuilderTest {
 
-    @Test
-    void check() {
-        ReferenceBuilder builder = new ReferenceBuilder();
-        builder.check(true);
-        Assertions.assertTrue(builder.build().isCheck());
-        builder.check(false);
-        Assertions.assertFalse(builder.build().isCheck());
-    }
+	@Test
+	void check() {
+		ReferenceBuilder builder = new ReferenceBuilder();
+		builder.check(true);
+		Assertions.assertTrue(builder.build().isCheck());
+		builder.check(false);
+		Assertions.assertFalse(builder.build().isCheck());
+	}
 
-    @Test
-    void init() {
-        ReferenceBuilder builder = new ReferenceBuilder();
-        builder.init(true);
-        Assertions.assertTrue(builder.build().isInit());
-        builder.init(false);
-        Assertions.assertFalse(builder.build().isInit());
-    }
+	@Test
+	void init() {
+		ReferenceBuilder builder = new ReferenceBuilder();
+		builder.init(true);
+		Assertions.assertTrue(builder.build().isInit());
+		builder.init(false);
+		Assertions.assertFalse(builder.build().isInit());
+	}
 
-    @Test
-    void generic() {
-        ReferenceBuilder builder = new ReferenceBuilder();
-        builder.generic(true);
-        Assertions.assertTrue(builder.build().isGeneric());
-        builder.generic(false);
-        Assertions.assertFalse(builder.build().isGeneric());
-    }
+	@Test
+	void generic() {
+		ReferenceBuilder builder = new ReferenceBuilder();
+		builder.generic(true);
+		Assertions.assertTrue(builder.build().isGeneric());
+		builder.generic(false);
+		Assertions.assertFalse(builder.build().isGeneric());
+	}
 
-    @Test
-    void generic1() {
-        ReferenceBuilder builder = new ReferenceBuilder();
-        builder.generic(GENERIC_SERIALIZATION_BEAN);
-        Assertions.assertEquals(GENERIC_SERIALIZATION_BEAN, builder.build().getGeneric());
-    }
+	@Test
+	void generic1() {
+		ReferenceBuilder builder = new ReferenceBuilder();
+		builder.generic(GENERIC_SERIALIZATION_BEAN);
+		Assertions.assertEquals(GENERIC_SERIALIZATION_BEAN,
+				builder.build().getGeneric());
+	}
 
-    @Test
-    void injvm() {
-        ReferenceBuilder builder = new ReferenceBuilder();
-        builder.injvm(true);
-        Assertions.assertTrue(builder.build().isInjvm());
-        builder.injvm(false);
-        Assertions.assertFalse(builder.build().isInjvm());
-    }
+	@Test
+	void injvm() {
+		ReferenceBuilder builder = new ReferenceBuilder();
+		builder.injvm(true);
+		Assertions.assertTrue(builder.build().isInjvm());
+		builder.injvm(false);
+		Assertions.assertFalse(builder.build().isInjvm());
+	}
 
-    @Test
-    void lazy() {
-        ReferenceBuilder builder = new ReferenceBuilder();
-        builder.lazy(true);
-        Assertions.assertTrue(builder.build().getLazy());
-        builder.lazy(false);
-        Assertions.assertFalse(builder.build().getLazy());
-    }
+	@Test
+	void lazy() {
+		ReferenceBuilder builder = new ReferenceBuilder();
+		builder.lazy(true);
+		Assertions.assertTrue(builder.build().getLazy());
+		builder.lazy(false);
+		Assertions.assertFalse(builder.build().getLazy());
+	}
 
-    @Test
-    void reconnect() {
-        ReferenceBuilder builder = new ReferenceBuilder();
-        builder.reconnect("reconnect");
-        Assertions.assertEquals("reconnect", builder.build().getReconnect());
-    }
+	@Test
+	void reconnect() {
+		ReferenceBuilder builder = new ReferenceBuilder();
+		builder.reconnect("reconnect");
+		Assertions.assertEquals("reconnect", builder.build().getReconnect());
+	}
 
-    @Test
-    void sticky() {
-        ReferenceBuilder builder = new ReferenceBuilder();
-        builder.sticky(true);
-        Assertions.assertTrue(builder.build().getSticky());
-        builder.sticky(false);
-        Assertions.assertFalse(builder.build().getSticky());
-    }
+	@Test
+	void sticky() {
+		ReferenceBuilder builder = new ReferenceBuilder();
+		builder.sticky(true);
+		Assertions.assertTrue(builder.build().getSticky());
+		builder.sticky(false);
+		Assertions.assertFalse(builder.build().getSticky());
+	}
 
-    @Test
-    void version() {
-        ReferenceBuilder builder = new ReferenceBuilder();
-        builder.version("version");
-        Assertions.assertEquals("version", builder.build().getVersion());
-    }
+	@Test
+	void version() {
+		ReferenceBuilder builder = new ReferenceBuilder();
+		builder.version("version");
+		Assertions.assertEquals("version", builder.build().getVersion());
+	}
 
-    @Test
-    void group() {
-        ReferenceBuilder builder = new ReferenceBuilder();
-        builder.group("group");
-        Assertions.assertEquals("group", builder.build().getGroup());
-    }
+	@Test
+	void group() {
+		ReferenceBuilder builder = new ReferenceBuilder();
+		builder.group("group");
+		Assertions.assertEquals("group", builder.build().getGroup());
+	}
 
-    @Test
-    void build() {
-        ReferenceBuilder builder = new ReferenceBuilder();
-        builder.check(true).init(false).generic(true).injvm(false).lazy(true).reconnect("reconnect").sticky(false)
-                .version("version").group("group").id("id").prefix("prefix");
+	@Test
+	void build() {
+		ReferenceBuilder builder = new ReferenceBuilder();
+		builder.check(true).init(false).generic(true).injvm(false).lazy(true)
+				.reconnect("reconnect").sticky(false).version("version")
+				.group("group").id("id").prefix("prefix");
 
-        ReferenceConfig config = builder.build();
-        ReferenceConfig config2 = builder.build();
+		AbstractReferenceConfig config = builder.build();
+		AbstractReferenceConfig config2 = builder.build();
 
-        Assertions.assertEquals("id", config.getId());
-        Assertions.assertEquals("prefix", config.getPrefix());
-        Assertions.assertTrue(config.isCheck());
-        Assertions.assertFalse(config.isInit());
-        Assertions.assertTrue(config.isGeneric());
-        Assertions.assertFalse(config.isInjvm());
-        Assertions.assertTrue(config.getLazy());
-        Assertions.assertFalse(config.getSticky());
-        Assertions.assertEquals("reconnect", config.getReconnect());
-        Assertions.assertEquals("version", config.getVersion());
-        Assertions.assertEquals("group", config.getGroup());
+		Assertions.assertEquals("id", config.getId());
+		Assertions.assertEquals("prefix", config.getPrefix());
+		Assertions.assertTrue(config.isCheck());
+		Assertions.assertFalse(config.isInit());
+		Assertions.assertTrue(config.isGeneric());
+		Assertions.assertFalse(config.isInjvm());
+		Assertions.assertTrue(config.getLazy());
+		Assertions.assertFalse(config.getSticky());
+		Assertions.assertEquals("reconnect", config.getReconnect());
+		Assertions.assertEquals("version", config.getVersion());
+		Assertions.assertEquals("group", config.getGroup());
 
-        Assertions.assertNotSame(config, config2);
-    }
+		Assertions.assertNotSame(config, config2);
+	}
 
-    private static class ReferenceBuilder extends AbstractReferenceBuilder<ReferenceConfig, ReferenceBuilder> {
+	private static class ReferenceBuilder extends
+			AbstractReferenceBuilder<AbstractReferenceConfig, ReferenceBuilder> {
 
-        public ReferenceConfig build() {
-            ReferenceConfig parameterConfig = new ReferenceConfig();
-            super.build(parameterConfig);
+		public AbstractReferenceConfig build() {
+			AbstractReferenceConfig parameterConfig = new MockReferenceConfig().instance;
+			super.build(parameterConfig);
 
-            return parameterConfig;
-        }
+			return parameterConfig;
+		}
 
-        @Override
-        protected ReferenceBuilder getThis() {
-            return this;
-        }
-    }
+		@Override
+		protected ReferenceBuilder getThis() {
+			return this;
+		}
+	}
 
-    private static class ReferenceConfig extends AbstractReferenceConfig {
-    }
+	private static class MockReferenceConfig {
+		public AbstractReferenceConfig instance;
+
+		public MockReferenceConfig() {
+			this.instance = Mockito.mock(AbstractReferenceConfig.class,
+					Mockito.CALLS_REAL_METHODS);
+		}
+	}
 }
