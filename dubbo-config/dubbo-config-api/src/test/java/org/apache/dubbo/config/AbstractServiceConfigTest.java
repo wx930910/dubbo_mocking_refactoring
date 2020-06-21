4439e75d200d37bb851d847020f131ce1cb3bc47
TwilightSparks <wx19930910@gmail.com>
2020-06-21 17:15:31 -0400

    Mock ServiceConfig.class in AbstractServiceConfigTest.java

--- dubbo-config/dubbo-config-api/src/test/java/org/apache/dubbo/config/AbstractServiceConfigTest.java
+++ dubbo-config/dubbo-config-api/src/test/java/org/apache/dubbo/config/AbstractServiceConfigTest.java
@@ -17,14 +17,6 @@
 
 package org.apache.dubbo.config;
 
-
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.apache.dubbo.common.constants.CommonConstants.EXPORTER_LISTENER_KEY;
 import static org.apache.dubbo.common.constants.CommonConstants.SERVICE_FILTER_KEY;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -35,150 +27,165 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class AbstractServiceConfigTest {
-    @Test
-    public void testVersion() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setVersion("version");
-        assertThat(serviceConfig.getVersion(), equalTo("version"));
-    }
+	@Test
+	public void testVersion() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setVersion("version");
+		assertThat(serviceConfig.getVersion(), equalTo("version"));
+	}
 
-    @Test
-    public void testGroup() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setGroup("group");
-        assertThat(serviceConfig.getGroup(), equalTo("group"));
-    }
+	@Test
+	public void testGroup() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setGroup("group");
+		assertThat(serviceConfig.getGroup(), equalTo("group"));
+	}
 
-    @Test
-    public void testDelay() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setDelay(1000);
-        assertThat(serviceConfig.getDelay(), equalTo(1000));
-    }
+	@Test
+	public void testDelay() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setDelay(1000);
+		assertThat(serviceConfig.getDelay(), equalTo(1000));
+	}
 
-    @Test
-    public void testExport() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setExport(true);
-        assertThat(serviceConfig.getExport(), is(true));
-    }
+	@Test
+	public void testExport() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setExport(true);
+		assertThat(serviceConfig.getExport(), is(true));
+	}
 
-    @Test
-    public void testWeight() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setWeight(500);
-        assertThat(serviceConfig.getWeight(), equalTo(500));
-    }
+	@Test
+	public void testWeight() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setWeight(500);
+		assertThat(serviceConfig.getWeight(), equalTo(500));
+	}
 
-    @Test
-    public void testDocument() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setDocument("http://dubbo.apache.org");
-        assertThat(serviceConfig.getDocument(), equalTo("http://dubbo.apache.org"));
-        Map<String, String> parameters = new HashMap<String, String>();
-        AbstractServiceConfig.appendParameters(parameters, serviceConfig);
-        assertThat(parameters, hasEntry("document", "http%3A%2F%2Fdubbo.apache.org"));
-    }
+	@Test
+	public void testDocument() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setDocument("http://dubbo.apache.org");
+		assertThat(serviceConfig.getDocument(),
+				equalTo("http://dubbo.apache.org"));
+		Map<String, String> parameters = new HashMap<String, String>();
+		AbstractServiceConfig.appendParameters(parameters, serviceConfig);
+		assertThat(parameters,
+				hasEntry("document", "http%3A%2F%2Fdubbo.apache.org"));
+	}
 
-    @Test
-    public void testToken() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setToken("token");
-        assertThat(serviceConfig.getToken(), equalTo("token"));
-        serviceConfig.setToken((Boolean) null);
-        assertThat(serviceConfig.getToken(), nullValue());
-        serviceConfig.setToken(true);
-        assertThat(serviceConfig.getToken(), is("true"));
-    }
+	@Test
+	public void testToken() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setToken("token");
+		assertThat(serviceConfig.getToken(), equalTo("token"));
+		serviceConfig.setToken((Boolean) null);
+		assertThat(serviceConfig.getToken(), nullValue());
+		serviceConfig.setToken(true);
+		assertThat(serviceConfig.getToken(), is("true"));
+	}
 
-    @Test
-    public void testDeprecated() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setDeprecated(true);
-        assertThat(serviceConfig.isDeprecated(), is(true));
-    }
+	@Test
+	public void testDeprecated() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setDeprecated(true);
+		assertThat(serviceConfig.isDeprecated(), is(true));
+	}
 
-    @Test
-    public void testDynamic() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setDynamic(true);
-        assertThat(serviceConfig.isDynamic(), is(true));
-    }
+	@Test
+	public void testDynamic() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setDynamic(true);
+		assertThat(serviceConfig.isDynamic(), is(true));
+	}
 
-    @Test
-    public void testProtocol() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        assertThat(serviceConfig.getProtocol(), nullValue());
-        serviceConfig.setProtocol(new ProtocolConfig());
-        assertThat(serviceConfig.getProtocol(), notNullValue());
-        serviceConfig.setProtocols(new ArrayList<>(Collections.singletonList(new ProtocolConfig())));
-        assertThat(serviceConfig.getProtocols(), hasSize(1));
-    }
+	@Test
+	public void testProtocol() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		assertThat(serviceConfig.getProtocol(), nullValue());
+		serviceConfig.setProtocol(new ProtocolConfig());
+		assertThat(serviceConfig.getProtocol(), notNullValue());
+		serviceConfig.setProtocols(new ArrayList<>(
+				Collections.singletonList(new ProtocolConfig())));
+		assertThat(serviceConfig.getProtocols(), hasSize(1));
+	}
 
-    @Test
-    public void testAccesslog() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setAccesslog("access.log");
-        assertThat(serviceConfig.getAccesslog(), equalTo("access.log"));
-        serviceConfig.setAccesslog((Boolean) null);
-        assertThat(serviceConfig.getAccesslog(), nullValue());
-        serviceConfig.setAccesslog(true);
-        assertThat(serviceConfig.getAccesslog(), equalTo("true"));
-    }
+	@Test
+	public void testAccesslog() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setAccesslog("access.log");
+		assertThat(serviceConfig.getAccesslog(), equalTo("access.log"));
+		serviceConfig.setAccesslog((Boolean) null);
+		assertThat(serviceConfig.getAccesslog(), nullValue());
+		serviceConfig.setAccesslog(true);
+		assertThat(serviceConfig.getAccesslog(), equalTo("true"));
+	}
 
-    @Test
-    public void testExecutes() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setExecutes(10);
-        assertThat(serviceConfig.getExecutes(), equalTo(10));
-    }
+	@Test
+	public void testExecutes() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setExecutes(10);
+		assertThat(serviceConfig.getExecutes(), equalTo(10));
+	}
 
-    @Test
-    public void testFilter() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setFilter("mockfilter");
-        assertThat(serviceConfig.getFilter(), equalTo("mockfilter"));
-        Map<String, String> parameters = new HashMap<String, String>();
-        parameters.put(SERVICE_FILTER_KEY, "prefilter");
-        AbstractServiceConfig.appendParameters(parameters, serviceConfig);
-        assertThat(parameters, hasEntry(SERVICE_FILTER_KEY, "prefilter,mockfilter"));
-    }
+	@Test
+	public void testFilter() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setFilter("mockfilter");
+		assertThat(serviceConfig.getFilter(), equalTo("mockfilter"));
+		Map<String, String> parameters = new HashMap<String, String>();
+		parameters.put(SERVICE_FILTER_KEY, "prefilter");
+		AbstractServiceConfig.appendParameters(parameters, serviceConfig);
+		assertThat(parameters,
+				hasEntry(SERVICE_FILTER_KEY, "prefilter,mockfilter"));
+	}
 
-    @Test
-    public void testListener() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setListener("mockexporterlistener");
-        assertThat(serviceConfig.getListener(), equalTo("mockexporterlistener"));
-        Map<String, String> parameters = new HashMap<String, String>();
-        parameters.put(EXPORTER_LISTENER_KEY, "prelistener");
-        AbstractServiceConfig.appendParameters(parameters, serviceConfig);
-        assertThat(parameters, hasEntry(EXPORTER_LISTENER_KEY, "prelistener,mockexporterlistener"));
-    }
+	@Test
+	public void testListener() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setListener("mockexporterlistener");
+		assertThat(serviceConfig.getListener(),
+				equalTo("mockexporterlistener"));
+		Map<String, String> parameters = new HashMap<String, String>();
+		parameters.put(EXPORTER_LISTENER_KEY, "prelistener");
+		AbstractServiceConfig.appendParameters(parameters, serviceConfig);
+		assertThat(parameters, hasEntry(EXPORTER_LISTENER_KEY,
+				"prelistener,mockexporterlistener"));
+	}
 
-    @Test
-    public void testRegister() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setRegister(true);
-        assertThat(serviceConfig.isRegister(), is(true));
-    }
+	@Test
+	public void testRegister() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setRegister(true);
+		assertThat(serviceConfig.isRegister(), is(true));
+	}
 
-    @Test
-    public void testWarmup() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setWarmup(100);
-        assertThat(serviceConfig.getWarmup(), equalTo(100));
-    }
+	@Test
+	public void testWarmup() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setWarmup(100);
+		assertThat(serviceConfig.getWarmup(), equalTo(100));
+	}
 
-    @Test
-    public void testSerialization() throws Exception {
-        ServiceConfig serviceConfig = new ServiceConfig();
-        serviceConfig.setSerialization("serialization");
-        assertThat(serviceConfig.getSerialization(), equalTo("serialization"));
-    }
+	@Test
+	public void testSerialization() throws Exception {
+		AbstractServiceConfig serviceConfig = new MockServiceConfig().instance;
+		serviceConfig.setSerialization("serialization");
+		assertThat(serviceConfig.getSerialization(), equalTo("serialization"));
+	}
 
+	private static class MockServiceConfig {
+		public AbstractServiceConfig instance = Mockito
+				.mock(AbstractServiceConfig.class, Mockito.CALLS_REAL_METHODS);
+	}
 
-    private static class ServiceConfig extends AbstractServiceConfig {
-
-    }
 }
